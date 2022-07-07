f9ecb079212f7d955346dbab5ff6048f77695acb
Lbelus <lorris.belus@icloud.com>
2022-07-07 17:13:17 +0200

    prevent_int_overflow

--- my_ngram.c
+++ my_ngram.c
@@ -24,23 +24,61 @@
 
 #define SIZE_MAX 128
 
+
+    // ref ITOA : https://www.strudel.org.uk/itoa/
+	char* their_itoa(int value, char* result, int base) {
+		// check that the base if valid
+		if (base < 2 || base > 36) { *result = '\0'; return result; }
+
+		char* ptr = result, *ptr1 = result, tmp_char;
+		int tmp_value;
+
+		do {
+			tmp_value = value;
+			value /= base;
+			*ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz" [35 + (tmp_value - value * base)];
+		} while ( value );
+
+		// Apply negative sign
+		if (tmp_value < 0) *ptr++ = '-';
+		*ptr-- = '\0';
+		while(ptr1 < ptr) {
+			tmp_char = *ptr;
+			*ptr--= *ptr1;
+			*ptr1++ = tmp_char;
+		}
+		return result;
+	}
+
 int main(int argc, char** argv){
-    char array[SIZE_MAX] = {0};
+    
+    
+    int array[SIZE_MAX] = {0};
 
     for(int x = 1; x < argc; x++){
         for(int y = 0; argv[x][y] != '\0'; y++){
             int decoy = (int)argv[x][y];
             array[decoy] += 1;
+            if(array[decoy] > 32767){
+                return 0; 
+            }
         }
     }
 
     for(int c = 0; c < 128;c++){
         if(array[c] > 0){
             int n =  array[c] + 48;
-            
             write(1, &c, 1);
             write(1,":",1);
-            write(1, &n, 1);
+            
+            if(n > 58){
+                n = n - 48;
+                char buffer[5] = {0};
+                their_itoa(n, buffer, 10);
+                write(1, buffer,5);
+            }else{
+                write(1, &n, 1);
+            }
             write(1,"\n",1);
         }
     }
